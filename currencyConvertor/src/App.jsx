010936b279e98580050b6be0ed8e7d6bb806ea39
GovindMishra8445 <govind6253ranjan@gmail.com>
2025-02-21 14:38:48 +0530

    Currency Convertor Application Create

---
+++ currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-600 text-black font-bold">Hello</div>
+    </>
+  );
+}
+
+export default App;
---
+++ currencyConvertor/src/components/InputBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InputBox() {
+  return (
+    <div className='input-box bg-amber-300 p-4'> 
+      InputBox
+    </div>
+  )
+}
+
+export default InputBox
